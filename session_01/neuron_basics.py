695af188f424453ab3b0de65bf309ce9d4270501
allenkang92 <ksw8841@knou.ac.kr>
2024-08-29 18:06:01 +0900

    1회차 공부 완료

---
+++ session_01/neuron_basics.py
@@ -0,0 +1,17 @@
+# 정수형 변수
+neuron_count = 100 
+
+# 부동소수점 변수
+threshold_voltage = - 55.0
+
+# 불리언 변수
+is_excited = False
+
+# 문자열 변수
+neuron_type = "pyramidal"
+
+# 변수 출력하기
+print("Number of neurons: ", neuron_count)
+print("Threshold voltage: ", threshold_voltage, "mV")
+print("Is the neuron excited? ", is_excited)
+print("Type of neuron : ", neuron_type)
